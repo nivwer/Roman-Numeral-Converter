416bb35ffe5c4ebb67f04505baccc37a3295687b
nivwer <nivwerr@gmail.com>
2022-11-05 14:39:23 -0300

    added converter.js

---
+++ converter.js
@@ -0,0 +1,31 @@
+function convertToRoman(num) {
+    const numerals = {
+      M:1000,
+      CM:900,
+      D:500,
+      CD:400,
+      C:100,
+      XC:90,
+      L:50,
+      XL:40,
+      X:10,
+      IX:9,
+      V:5,
+      IV:4,
+      I:1
+    }
+  
+    let roman = ''
+  
+    for (let i in numerals) {
+      while(num >= numerals[i]) {
+        roman += i
+        num -= numerals[i]
+      }
+    }
+  
+  
+    return roman;
+  }
+  
+  convertToRoman(2);
